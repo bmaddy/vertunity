49a632f5fae3c1d85c88979414199f08de0589ec
Brian Maddy <brian@brianmaddy.com>
2013-06-01 19:31:24 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to vertunity
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/vertunity/core_test.clj
@@ -0,0 +1,7 @@
+(ns vertunity.core-test
+  (:use clojure.test
+        vertunity.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
